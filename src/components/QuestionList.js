0bfc7930551c89fc936dd7b0104d652977169801
Marion Nabulobi <arinabulobi@gmail.com>
2024-11-14 14:48:01 +0300

    add fetch crud lab

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => { 
+    fetch("http://localhost:4000/questions") 
+    .then((response) => response.json()) 
+    .then((data) => setQuestions(data)); 
+  }, []);
+
+function deleteQuestion(id) {
+  setQuestions(questions.filter(question => question.id !== id));
+}
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+      <ul>
+      {questions.map((question) => (
+         <QuestionItem key={question.id} question={question} onDeleteQuestion={deleteQuestion} />
+      ))}
+      </ul>
+      </section>
   );
 }
 
